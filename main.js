5b1a4e815b6218f6bae8e4bf588902d9c0dfaf94
shvabriik <alisaisaeva479@gmail.com>
2024-02-10 18:03:20 +0200

    quest2

---
+++ main.js
@@ -0,0 +1,54 @@
+let start =document.getElementById("start")
+let result =document.getElementById("result")
+let main=document.getElementById("main")
+
+let allQuestion = [
+    {
+        question: "2+2",
+        correct: 4,
+        answers: [4, 5, 10, 12],
+    },
+    {
+        question: "4+4",
+        correct: 8,
+        answers: [1, 5, 8, 12],
+    },
+];
+
+let currentQuestion = 0;
+let countSuccess = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
